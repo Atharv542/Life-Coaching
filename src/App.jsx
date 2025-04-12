cb9a41c5c609732d9be2b968ca975cd0d5bbd52c
Atharv542 <162612781+Atharv542@users.noreply.github.com>
2025-04-12 16:42:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
                 
                 <Testimonials />
                 </div>
-                {/*<Pricing/>*/}
+                <Pricing/>
                 
                 <Footer />
               </>
